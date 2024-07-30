c4ca38dd657392d83672a578a3136529ed02ae67
Anand Jain <anandj@uchicago.edu>
2024-07-30 12:47:19 -0700

    corrected/working nms correctly gets one box

--- app/src/main/java/com/example/send_angle2/MainActivity.kt
+++ app/src/main/java/com/example/send_angle2/MainActivity.kt
@@ -42,6 +42,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asAndroidBitmap
@@ -108,6 +109,7 @@ fun MyApp(ortSession: OrtSession?) {
     var angle2 by remember { mutableStateOf("") }
     var responseText by remember { mutableStateOf("Press the button to set servo angles") }
     var resultsText by remember { mutableStateOf("Results will be shown here") }
+//    var filteredBoxes by remember { mutableStateOf<List<List<<Float>>(emptyList(emptyList())) }
 
     MaterialTheme {
         Surface {
@@ -234,21 +236,19 @@ fun MyApp(ortSession: OrtSession?) {
                             val inputName = ortSession.inputNames.iterator().next()
                             val feeds = mapOf(inputName to tensor)
                             val results = ortSession.run(feeds)
-//                            val rawOutput = results?.get(0)?.value
-                            val rawOutputBoxes = ((results.get(1)?.value) as Array<Array<FloatArray>>)[0]
-                            Log.d("ORTOutput", "Raw Output: ${rawOutputBoxes.toString()}")
-//                            Log.d("ORTOutputBoxes", "Raw Output Boxes: ${rawOutputBoxes.joinToString()}")
+                            val scores =
+                                (results?.get(0)?.value as Array<Array<FloatArray>>)[0]
+                            val boxes =
+                                ((results.get(1)?.value) as Array<Array<FloatArray>>)[0]
 
                             resultsText = "Results: " + results.toString()
-//                            resultsText += results.get(0).info
                             resultsText += results.get("scores").get().info
                             resultsText += results.get("boxes").get().info
                             resultsText += "\n"
-//                            val subArray = rawOutputBoxes[0].sliceArray(0..3)
-                            val foo = listOf(rawOutputBoxes[0][0], rawOutputBoxes[0][1], rawOutputBoxes[0][2], rawOutputBoxes[0][3])
-                            resultsText += foo
+                            val filteredBoxes = nonMaxSuppression(boxes, scores, 0.9f, 0.7f)
 
-//                            resultsText += results.get(1).info
+                            resultsText += "Filtered Boxes count: " + filteredBoxes.size.toString()
+                            resultsText += filteredBoxes.toString()
                         }
 
                     }
@@ -263,6 +263,14 @@ fun MyApp(ortSession: OrtSession?) {
     }
 }
 
+//fun to_box(raw_output: FloatArray, scores: FloatArray): BBox {
+//    val x1 = raw_output[0]
+//    val y1 = raw_output[1]
+//    val x2 = raw_output[2]
+//    val y2 = raw_output[3]
+//    val score = scores[0]
+//    return BBox(x1, y1, x2, y2, score)
+//}
 
 private fun createTensorFromImage(imageBitmap: androidx.compose.ui.graphics.ImageBitmap): OnnxTensor {
     val width = 320
@@ -274,12 +282,13 @@ private fun createTensorFromImage(imageBitmap: androidx.compose.ui.graphics.Imag
     canvas.drawBitmap(
         imageBitmap.asAndroidBitmap(),
         null,
-        android.graphics.Rect(0, 0, width, height),
+        Rect(0, 0, width, height),
         null
     )
 
     val pixels = IntArray(width * height)
     bitmap.getPixels(pixels, 0, width, 0, 0, width, height)
+    Log.d("ORTImageClassifier", "pixels: ${pixels.size}")
 
     for (i in 0 until height) {
         for (j in 0 until width) {
@@ -294,45 +303,74 @@ private fun createTensorFromImage(imageBitmap: androidx.compose.ui.graphics.Imag
         }
     }
 
+    // logd the first couple pixels
+    for (i in 0 until 2)
+        for (j in 0 until 2)
+            Log.d("ORTImageClassifier",
+                "pixel: ${input[(0 * height + i) * width + j]} ${input[(1 * height + i) * width + j]} ${input[(2 * height + i) * width + j]}")
+
+
+
     val env = OrtEnvironment.getEnvironment()
     val shape = longArrayOf(1, 3, height.toLong(), width.toLong())
-    return OnnxTensor.createTensor(env, FloatBuffer.wrap(input), shape)
+    val tensor =OnnxTensor.createTensor(env, FloatBuffer.wrap(input), shape)
+
+    return tensor
+
 }
-
-data class Box(val x1: Float, val y1: Float, val x2: Float, val y2: Float, val score: Float)
-
-fun intersectionOverUnion(boxA: Box, boxB: Box): Float {
-    val xA = maxOf(boxA.x1, boxB.x1)
-    val yA = maxOf(boxA.y1, boxB.y1)
-    val xB = minOf(boxA.x2, boxB.x2)
-    val yB = minOf(boxA.y2, boxB.y2)
-
-    val interArea = maxOf(0f, xB - xA) * maxOf(0f, yB - yA)
-    val boxAArea = (boxA.x2 - boxA.x1) * (boxA.y2 - boxA.y1)
-    val boxBArea = (boxB.x2 - boxB.x1) * (boxB.y2 - boxB.y1)
-
-    return interArea / (boxAArea + boxBArea - interArea)
-}
-
-fun nonMaxSuppression(boxes: List<Box>, scoreThreshold: Float, iouThreshold: Float): List<Box> {
-    val filteredBoxes = mutableListOf<Box>()
+fun nonMaxSuppression(
+    boxes: Array<FloatArray>,
+    scores: Array<FloatArray>,
+    scoreThreshold: Float,
+    iouThreshold: Float
+): List<List<Float>> {
+    val filteredBoxes = mutableListOf<List<Float>>()
 
     // Filter out boxes with low scores
-    val candidates = boxes.filter { it.score > scoreThreshold }.sortedByDescending { it.score }.toMutableList()
+    val candidates = mutableListOf<List<Float>>()
+    for (i in scores.indices) {
+        if (scores[i][1] > scoreThreshold) {
+            candidates.add(
+                listOf(
+                    boxes[i][0],
+                    boxes[i][1],
+                    boxes[i][2],
+                    boxes[i][3],
+                    scores[i][1]
+                )
+            )
+        }
+    }
+
+    // Sort candidates by score in descending order
+    candidates.sortByDescending { it[4] }
 
     // Perform Non-Maximum Suppression
     while (candidates.isNotEmpty()) {
-        val bestBox = candidates.removeAt(0)
-        filteredBoxes.add(bestBox)
+        val (x1, y1, x2, y2, score) = candidates.removeAt(0)
+        filteredBoxes.add(listOf(x1, y1, x2, y2, score))
 
-        val iterator = candidates.iterator()
-        while (iterator.hasNext()) {
-            val box = iterator.next()
-            if (intersectionOverUnion(bestBox, box) >= iouThreshold) {
-                iterator.remove()
-            }
+        candidates.removeAll { box ->
+            val iou = intersectionOverUnion(
+                listOf(x1, y1, x2, y2),
+                listOf(box[0], box[1], box[2], box[3])
+            )
+            iou >= iouThreshold
         }
     }
 
     return filteredBoxes
+}
+
+fun intersectionOverUnion(boxA: List<Float>, boxB: List<Float>): Float {
+    val xA = maxOf(boxA[0], boxB[0])
+    val yA = maxOf(boxA[1], boxB[1])
+    val xB = minOf(boxA[2], boxB[2])
+    val yB = minOf(boxA[3], boxB[3])
+
+    val interArea = maxOf(0f, xB - xA) * maxOf(0f, yB - yA)
+    val boxAArea = (boxA[2] - boxA[0]) * (boxA[3] - boxA[1])
+    val boxBArea = (boxB[2] - boxB[0]) * (boxB[3] - boxB[1])
+
+    return interArea / (boxAArea + boxBArea - interArea)
 }
